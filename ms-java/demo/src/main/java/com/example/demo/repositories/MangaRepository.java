7cc1f0c33c8e9a18d971154862e06f6ee7faf4f6
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2023-09-17 10:31:51 -0300

    Delete ms-java/demo directory

--- ms-java/demo/src/main/java/com/example/demo/repositories/MangaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Manga;
-
-public interface MangaRepository extends JpaRepository<Manga, Long>{
-    
-}
--- ms-java/demo/src/main/java/com/example/demo/services/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.services.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
