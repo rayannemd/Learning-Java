64069222a7dace9f0ea03c554dfef9e8b82ac052
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2023-09-17 10:39:38 -0300

    Setting up application

---
+++ learningspring/ms-java/demo/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return (List<User>) userRepository.findAll();
+    }
+}
